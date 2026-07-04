c4c2247cb654b7caf9e74a88d37a90ebb10de396
Erfan Khatamy <mr.erfan.khatami@gmail.com>
2026-07-04 20:54:55 +0330

    added addTodos and loadTodos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 //  Todo app Functionality
+let todos = [];
 
 // add todos
-function addTodo() {}
+function addTodo() {
+  const newTodo = {
+    id: crypto.randomUUID(),
+    text: document.getElementById("title-input").value,
+    desc: document.getElementById("description-input").value,
+    //     priority: document.getElementById("priority-input").value,
+    //     priorityColor: document.getElementById("priority-color-input").value,
+    completed: false,
+    editing: false,
+  };
+
+  todos = [...todos, newTodo];
+}
 
 // render todo list
 function renderTodo() {}
 
 // Load Todos from local storage
-function loadTodos() {}
+function loadTodos() {
+  todos = JSON.parse(localStorage.getItem("todos")) || [];
+}
 
 // Save Todos to local storage
 function saveTodos() {}
@@ -20,3 +35,9 @@ function deleteIncompleteTodo() {}
 
 // delete completed todo
 function deleteCompletedTodo() {}
+
+const submitButton = document.getElementById("sumbit-button");
+submitButton.addEventListener("click", () => {
+  addTodo();
+  console.log(todos);
+});
