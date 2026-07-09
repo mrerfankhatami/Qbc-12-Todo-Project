b9d7d482b884f2cf19cfc10d824ababe22a2967f
Erfan Khatamy <mr.erfan.khatami@gmail.com>
2026-07-09 19:10:39 +0330

    implemented the completed task and fixed the counter task

--- js/main.js
+++ js/main.js
@@ -25,6 +25,7 @@ function renderTodo() {
   );
   incompleteContainer.innerHTML = "";
   todos.forEach((todo) => {
+    if (todo.completed) return;
     incompleteContainer.innerHTML += ` <div class="flex flex-col">
       <div
         class="flex items-center bg-white dark:bg-[#091120] rounded-xl border border-[#E9E9E9] dark:border-gray-700 mb-3 relative">
@@ -54,7 +55,7 @@ function renderTodo() {
           </div>
         <div class="flex-1 py-3 px-4">
           <div class="flex items-start gap-4">
-            <input type="checkbox" id="task-check" checked
+            <input type="checkbox" id="task-check" onClick="completeTask('${todo.id}')"
               class="w-5 h-5 rounded-[5px] border border-[#CCCCCC] appearance-none checked:bg-[#007BFF] checked:border-[#007BFF] focus:ring-2 focus:ring-[#007BFF]" />
             <div class="flex flex-col gap-1">
              <div class="flex gap-3">
@@ -158,6 +159,52 @@ function renderTodo() {
 
   checkNoTaskImage();
   taskCounterUpdate();
+
+  const completedTaskContainer = document.getElementById(
+    "completed-task-container",
+  );
+  completedTaskContainer.innerHTML = ``;
+  todos.forEach((todo) => {
+    if (!todo.completed) return;
+
+    completedTaskContainer.innerHTML += `
+            <div
+          class="w-full min-h-16 border border-[#E9E9E9] rounded-xl px-4 py-3 flex items-center justify-between dark:bg-[#091120] dark:border-none relative">
+          <div class="w-1 h-11 md:h-12  bg-[${todo.priorityColor}] rounded-l-lg absolute right-0"></div>
+          <div class="flex justify-start items-center gap-4 md:gap-3 max-w-[700px]">
+            <input type="checkbox" id="task-check" checked 
+              class="w-5 h-5 rounded-[5px] border border-[#CCCCCC] appearance-none checked:bg-[#007BFF] checked:border-[#007BFF] focus:ring-2 focus:ring-[#007BFF]" onClick="completeTask('${todo.id}')"/>
+            <div class="max-w-[550px]">
+              <label for="task-check"
+                class="tex-[14px] text-[#323233] font-semibold line-through md:text-[16px] dark:text-white">
+              ${todo.text}
+            </div>
+          </div>
+          <div
+            class="hidden absolute w-10 h-8 border border-[#EBEDEF] rounded-lg p-1  items-center justify-around left-10 top-4 dark:bg-[#0B192D] dark:border-[#293242]">
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="size-6 stroke-[#5C5F61] dark:stroke-[#FFFFFF] hover:stroke-red-600 hover:cursor-pointer">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+            </svg>
+
+
+           
+
+
+
+          </div>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" onclick="openDeleteEdit(this)"
+              stroke="currentColor" class="size-7 text-[#525252] dark:text-white cursor-pointer">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+            </svg>
+          </div>
+        </div>
+`;
+  });
 }
 
 // // Load Todos from local storage
@@ -276,10 +323,12 @@ window.closePriority = function () {
 function taskCounterUpdate() {
   const taskCounter = document.getElementById("task-counter");
 
+  const completedTodos = todos.filter((todo) => !todo.completed);
+
   taskCounter.textContent =
-    todos.length === 0
+    completedTodos.length === 0
       ? "تسکی برای امروز نداری!"
-      : `${todos.length} تسک را باید انجام دهید.`;
+      : `${completedTodos.length} تسک را باید انجام دهید.`;
 }
 
 window.openDeleteEdit = function (button) {
@@ -290,4 +339,13 @@ window.openDeleteEdit = function (button) {
 
 window.deleteTodo = function (id) {
   deleteIncompleteTodo(id);
+};
+
+window.completeTask = function (id) {
+  const todo = todos.find((todo) => todo.id === id);
+  if (!todo) return;
+
+  todo.completed = !todo.completed;
+
+  renderTodo();
 };
