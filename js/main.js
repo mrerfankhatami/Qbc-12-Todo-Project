5bf022ede1e1d45a0be7d7ad5ab0e8c03e9e4053
Erfan Khatamy <mr.erfan.khatami@gmail.com>
2026-07-09 17:49:25 +0330

    fixed delete task

--- js/main.js
+++ js/main.js
@@ -30,10 +30,11 @@ function renderTodo() {
         class="flex items-center bg-white dark:bg-[#091120] rounded-xl border border-[#E9E9E9] dark:border-gray-700 mb-3 relative">
         <div class="w-1 bg-[${todo.color}] self-stretch rounded-l-full my-3"></div>
         <div
-            class="hidden absolute w-20 h-8 border border-[#EBEDEF] rounded-lg p-1 items-center justify-around left-5 top-2 dark:bg-[#0B192D] dark:border-[#293242]">
+        
+      class="task-menu hidden absolute w-20 h-8 border border-[#EBEDEF] rounded-lg p-1 items-center justify-around left-5 top-2 dark:bg-[#0B192D] dark:border-[#293242]">
 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="currentColor" class="size-6 stroke-[#5C5F61] dark:stroke-[#FFFFFF] hover:stroke-red-600 hover:cursor-pointer">
+              stroke="currentColor" id="delete-btn" class="size-6 stroke-[#5C5F61] dark:stroke-[#FFFFFF] hover:stroke-red-600 hover:cursor-pointer" onClick="deleteTodo('${todo.id}')">
               <path stroke-linecap="round" stroke-linejoin="round"
                 d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
             </svg>
@@ -66,7 +67,7 @@ function renderTodo() {
             </div>
           </div>
         </div>
-        <button class="flex flex-col gap-[3px] items-center justify-center py-3 px-2 self-start cursor-pointer">
+        <button class="flex flex-col gap-[3px] items-center justify-center py-3 px-2 self-start cursor-pointer" onclick="openDeleteEdit(this)">
           <span class="w-1 h-1 rounded-full bg-[#525252] dark:bg-gray-400"></span>
           <span class="w-1 h-1 rounded-full bg-[#525252] dark:bg-gray-400"></span>
           <span class="w-1 h-1 rounded-full bg-[#525252] dark:bg-gray-400"></span>
@@ -280,3 +281,13 @@ function taskCounterUpdate() {
       ? "تسکی برای امروز نداری!"
       : `${todos.length} تسک را باید انجام دهید.`;
 }
+
+window.openDeleteEdit = function (button) {
+  const menu = document.querySelector(".task-menu");
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("flex");
+};
+
+window.deleteTodo = function (id) {
+  deleteIncompleteTodo(id);
+};
